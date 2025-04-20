7067447ab9911b26d53605323a933c154e36d0cc
John Fryar <theintermancer@gmail.com>
2025-04-20 17:41:03 -0500

    Updated the RepoFactory and the InMemOrgRepoTest to using Claude to align with the context.

--- src/main/java/com/intermancer/gaiaf/core/organism/repo/OrganismRepositoryFactory.java
+++ src/main/java/com/intermancer/gaiaf/core/organism/repo/OrganismRepositoryFactory.java
@@ -1,9 +1,14 @@
 package com.intermancer.gaiaf.core.organism.repo;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OrganismRepositoryFactory {
     private static final InMemoryOrganismRepository INSTANCE = new InMemoryOrganismRepository();
 
-    public static OrganismRepository getInstance() {
+    @Bean
+    public OrganismRepository getInMemoryOrganismRepository() {
         return INSTANCE;
     }
 }
